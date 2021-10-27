4e650f5c3cd2ac510339212ad89410c2dc502e63
ferialops <90615135+ferialops@users.noreply.github.com>
2021-10-27 14:07:42 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/kunakhova_a_y/Main.java
@@ -0,0 +1,41 @@
+package ru.vsu.cs.kunakhova_a_y;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+
+        int n = enterN();
+
+        int result = findAmount(n);
+        System.out.print(result);
+    }
+
+    static int enterN() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n = ");
+        int value = sc.nextInt();
+        if (value <= 0) {
+            System.out.print("Value can be only more then 0");
+            System.exit(1);
+        }
+        return value;
+    }
+
+    static int findAmount(int n) {
+        int firstNum = 0;
+        int secondNum = 1;
+        int amount = 0;
+        while ((firstNum + secondNum) <= n) {
+            int fibNum = firstNum + secondNum;
+            if ((fibNum % 2) == 0) {
+                amount = amount + fibNum;
+            }
+            firstNum = secondNum;
+            secondNum = fibNum;
+        }
+        return amount;
+    }
+}
